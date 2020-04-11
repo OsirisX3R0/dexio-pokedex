23c01f57cc5e7213c7fe740e45d5338f2a67d365
Richard Harris <richardharris916@gmail.com>
2020-04-11 16:35:51 -0500

    Fixed damage logic

--- src/Components/Pokemon/PokemonDamage.js
+++ src/Components/Pokemon/PokemonDamage.js
@@ -1,10 +1,14 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import BlockUi from 'react-block-ui';
 import Loader from 'react-loaders';
 import { PokemonContext } from '../../Context/PokemonContext';
 
 const PokemonDamage = () => {
-    const { types, loading } = useContext(PokemonContext);
+    const { relations, loading } = useContext(PokemonContext);
+    
+
+        //return <div></div>;
+    //}
 
     return (
         <BlockUi blocking={loading} loader={<Loader type="ball-grid-pulse" />}>
--- src/Context/PokemonContext.js
+++ src/Context/PokemonContext.js
@@ -10,6 +10,7 @@ export const PokemonProvider = ({ children }) => {
     const [pokemon, setPokemon] = useState(null);
     const [statTotal, setStatTotal] = useState(null);
     const [types, setTypes] = useState([]);
+    const [relations, setRelations] = useState({});
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
@@ -28,27 +29,106 @@ export const PokemonProvider = ({ children }) => {
         let promises = [];
 
         if (pokemon) {
-            debugger;
+            //debugger;
             pokemon.types.forEach(type => {
                 promises = [...promises, getType(type.type.name)]
             })            
 
             Promise.all(promises)
                 .then(res => {
-                    setTypes(res.map(t => t.data));
+                    let typesArr = res.map(t => t.data);
+                    setTypes(typesArr);
+                    getDamageRelations(typesArr);
                 })
         }
     }, [pokemon])
+
+    const getDamageRelations = typeRelations => {
+        if (typeRelations.length === 1)
+            return typeRelations[0].damage_relations;
+
+        let fullRelations = {};
+
+        if (typeRelations.length > 0) {
+            fullRelations = {
+                double_damage_from: [...typeRelations[0].damage_relations.double_damage_from, ...typeRelations[1].damage_relations.double_damage_from],
+                double_damage_to: [...typeRelations[0].damage_relations.double_damage_to, ...typeRelations[1].damage_relations.double_damage_to],
+                half_damage_from: [...typeRelations[0].damage_relations.half_damage_from, ...typeRelations[1].damage_relations.half_damage_from],
+                half_damage_to: [...typeRelations[0].damage_relations.half_damage_to, ...typeRelations[1].damage_relations.half_damage_to],
+                no_damage_from: [...typeRelations[0].damage_relations.no_damage_from, ...typeRelations[1].damage_relations.no_damage_from],
+                no_damage_to: [...typeRelations[0].damage_relations.no_damage_to, ...typeRelations[1].damage_relations.no_damage_to],
+            };
+
+            //let weakTo = ;
+
+            setRelations({
+                // strongAgainst: fullRelations.double_damage_to.map(type => {
+                //     return {
+                //         name: type.name,
+                //         power: fullRelations.double_damage_to.filter(r => r.name === type.name).length * 2
+                //     }
+                // }),
+                weakTo: fullRelations.double_damage_from
+                    .map(type => {
+                        return {
+                            name: type.name,
+                            power: fullRelations.double_damage_from.filter(r => r.name === type.name).length * 2
+                        }
+                    })
+                    .filter((type, index, self) => 
+                        index === self.findIndex((t) => (
+                            t.name === type.name && t.power === type.power
+                        ))
+                    ),
+                resistantTo: fullRelations.half_damage_from
+                    .map(type => {
+                        return {
+                            name: type.name,
+                            power: fullRelations.half_damage_from.filter(r => r.name === type.name).length === 1
+                                ? "1/2"
+                                : "1/4"
+                        }
+                    })
+                    .filter((type, index, self) => 
+                        index === self.findIndex((t) => (
+                            t.name === type.name && t.power === type.power
+                        ))
+                    ),
+                // weakAgainst: fullRelations.half_damage_to.map(type => {
+                //     return {
+                //         name: type.name,
+                //         power: fullRelations.half_damage_to.filter(r => r.name === type.name).length / 2
+                //     }
+                // }),
+                immuneTo: fullRelations.no_damage_from
+                    .map(type => {
+                        return {
+                            name: type.name,
+                            power: 0
+                        }
+                    })
+                    .filter((type, index, self) => 
+                        index === self.findIndex((t) => (
+                            t.name === type.name && t.power === type.power
+                        ))
+                    )
+            })
+        }
+    }
 
     return (
         <PokemonContext.Provider value={{
             name,
             pokemon,
             statTotal,
-            types,
+            relations,
             loading
         }}>
             {children}
         </PokemonContext.Provider>
     )
 }
+
+const isNormal = r => r.name === "normal";
+
+const isFighting = r => r.name === "fighting";
