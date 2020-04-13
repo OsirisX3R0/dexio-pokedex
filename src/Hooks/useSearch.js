8349ec3d4b6fd0c633133964ac5b0b74dc8bc42d
Richard Harris <richardharris916@gmail.com>
2020-04-13 18:39:45 -0500

    Fixed search hook

--- src/Hooks/useSearch.js
+++ src/Hooks/useSearch.js
@@ -1,11 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 const useSearch = (query, collection, property) => {
+    const [searchQuery, setSearchQuery] = useState(query);
     const [results, setResults] = useState(null);
+    const [original, setOriginal] = useState([...collection]);
 
-    const search = () => {
-        setResults(collection.filter(i => i[property].includes(query)))
-    }
+    const search = useCallback(() => {
+        if (searchQuery === '') {
+            setResults(original);
+            return;
+        }
+        let searchResults = collection.filter(i => i[property].includes(searchQuery));
+        setResults(searchResults);
+    }, [searchQuery, collection, original, property])
+
+    useEffect(() => {
+        setOriginal(collection)
+    }, [collection])
+
+    useEffect(() => {
+        setSearchQuery(query);
+        search();
+    }, [query, search])
 
     return [results, search];
 }
