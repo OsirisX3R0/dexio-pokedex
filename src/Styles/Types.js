80a71ebc99a82cd4560fc4a43a8aef0d77e1262e
Richard Harris <richardharris916@gmail.com>
2020-04-22 15:35:41 -0500

    Updated styles for type chart

--- src/Styles/Types.js
+++ src/Styles/Types.js
@@ -16,26 +16,33 @@ const ChartHeader = styled.div`
     color: ${light};
     border-radius: 5px;
     text-transform: capitalize;
+    position: sticky;
     display: grid;
     align-items: center;
     justify-content: center;
 `;
 
 export const ChartHeadCorner = styled.div`
+    background-color: ${light};
     text-transform: capitalize;
+    position: sticky;
+    top: 0;
+    left: 0;
+    z-index: 100;
 `;
 
 export const ChartDefenseHeader = styled(ChartHeader)`
-
+    top: 0;
 `;
 
 export const ChartOffenseHeader = styled(ChartHeader)`
-
+    left: 0;
 `;
 
 export const ChartCell = styled.div`
     background-color: ${props => getDamageValueBackground(props.damageValue)};
     color: ${props => (props.damageValue === 1 ? dark : light)};
+    border-radius: 5px;
     padding: .25rem;
     display: grid;
     align-items: center;
