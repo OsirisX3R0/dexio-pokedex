34b646e56cf86fd90d9a932b9c26feef725ad1d4
Richard Harris <richardharris916@gmail.com>
2020-04-28 17:43:22 -0500

    Removed block-ui css import

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import AllMoves from './Components/Moves/AllMoves';
 import Move from './Components/Moves/Move';
 import TypeChart from './Components/Types/TypeChart';
 import './App.scss';
-import 'react-block-ui/style.css';
 import 'react-placeholder/lib/reactPlaceholder.css';
 
 const App = () => {
